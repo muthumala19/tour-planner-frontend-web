278cd9e440668e9c55d19a2c8d76569608b42ae8
Pasindu Muthumala <lgpmuthumala@gmail.com>
2023-09-03 22:16:39 +0530

    Make landing screen clear and responsive

--- src/screens/landing_screen.jsx
+++ src/screens/landing_screen.jsx
@@ -1,12 +1,21 @@
 import './landing_screen.css'
+import Footer from "../components/Footer";
 import NavBarComponent from "../components/navbar_component";
 
 export default function LandingScreen() {
-    return <div className={"landing_screen"}>
-        <div className={'landing_screen_navbar'}><NavBarComponent/></div>
-        <div id='home' className={'section first'}></div>
-        <div id='sign_up' className={'section'}>sign up</div>
-        <div id='about_us' className={'section'}>about us</div>
-        <div id='contact_us' className={'section'}>contact us</div>
-    </div>
+    const navbarItems = [
+        {label: 'Home', href: '#home'},
+        {label: 'Sign Up', href: '#sign_up'},
+        {label: 'About Us', href: '#about_us'},
+        {label: 'Contact Us', href: '#contact_us'},
+    ];
+    return (
+        <div className={"landing_screen"}>
+            <div className={'navbar_position'}><NavBarComponent items={navbarItems}/></div>
+            <div id='home' className={'section first'}></div>
+            <div id='sign_up' className={'section'}>sign up</div>
+            <div id='about_us' className={'section'}>about us</div>
+            <div id='contact_us' className={'section'}>contact us</div>
+            <div><Footer/></div>
+        </div>);
 }
