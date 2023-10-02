3261cb483da04c73e0e3e9a0e61e1ca86456daaf
ludeesha20 <ludeeshananayakkara.20@gmail.com>
2023-10-02 11:01:37 +0530

    modifying for connect

--- src/App.js
+++ src/App.js
@@ -1,32 +1,23 @@
 import './App.css';
-
-import LandingScreen from "./screens/landing_screen";
+import '../src/screens/auth_screen'
 import Destination from './screens/Destinations.jsx';
 import Accomodation from './screens/accomodation_screen.jsx';
-import './App.css';
-import '../src/screens/auth_screen'
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import AuthScreen from "./screens/auth_screen";
 import Choose_ride from "./screens/explore_your_Ideal_ride.jsx";
 import PaymentPage from "./screens/Payment_screen.jsx";
 
-
 function App() {
     return (
-
       <div className="App">
-
       <BrowserRouter>
-
-
           <Routes>
-              <Route exact path='/' element={<LandingScreen/>}></Route>
               <Route exact path='/destination' element={<Destination/>}></Route>
               <Route exact path='/payment' element={<PaymentPage/>}></Route>
               <Route exact path='/accomodation' element={<Accomodation/>}></Route>
               <Route exact path='/explore_your_ideal_ride' element={<Choose_ride/>}></Route>
               <Route exact path='/sign_in' element={<AuthScreen isSignIn={true}/>}></Route>
-              <Route exact path='/sign_up' element={<AuthScreen isSignIn={false}/>}></Route>
+              <Route exact path='/sign_up' element={<AuthScreen isSignIn={false}/>}></Route>            
           </Routes>
       </BrowserRouter>
   </div>
--- src/components/explore_your_Ideal_ride_components.jsx
+++ src/components/explore_your_Ideal_ride_components.jsx
@@ -1,8 +1,8 @@
 import React from "react"
 import Date_box_component from "./Date_box_component.jsx";
-import "../components/Destination_buttons.css"
-import Location_btn_component from "../components/Location_btn_component.jsx";
-import PriceRange_component from '../components/PriceRange_component.jsx';
+
+import Location_btn_component from "./Location_btn_component.jsx";
+import PriceRange_component from './PriceRange_component.jsx';
 import Inc_Dec_btn from "./IncrementBtn_component.jsx";
 import "./accomodation_pge_component.css";
 import "./explore_your_Ideal_ride_components.css";
@@ -30,8 +30,8 @@ export default function Explore_your_Ideal_ride(){
       {/* Date, Location, Accomodation, Price Range, Explore, Skip Accomodation buttons */}
 
       <div className='row'>
-        <div className="col-12 col-sm-6 col-lg-4"><Date_box_component text="starting date"/></div> 
-        <div className="col-12 col-sm-6 col-lg-4"><Date_box_component text="end date"/></div>
+        <div className="col-12 col-sm-6 col-lg-4"><Date_box_component text="Starting date"/></div> 
+        <div className="col-12 col-sm-6 col-lg-4"><Date_box_component text="End date"/></div>
         <div className="col-12 col-sm-12 col-lg-4"><Location_btn_component/></div> 
       </div>
       <div className="row accomodation" >
