7c98e6b797d60dd61e8305dd134e5e6430ee2c6a
Kavindu Nilshan <kavindunilshanliyanage@gmail.com>
2023-10-12 13:40:42 +0530

    Made more general

--- src/backend/hotelGeneration.js
+++ src/backend/hotelGeneration.js
@@ -1,33 +1,31 @@
 import axios from "axios";
 
-const options = {
-    method: 'GET',
-    url: 'https://booking-com.p.rapidapi.com/v1/hotels/search-by-coordinates',
-    params: {
-        units: 'metric',
-        room_number: '1',
-        longitude: '79.8612',
-        latitude: '6.9271',
-        filter_by_currency: 'AED',
-        order_by: 'class_descending',
-        locale: 'en-gb',
-        checkout_date: '2023-10-28',
-        adults_number: '2',
-        checkin_date: '2023-10-27',
-        children_ages: '5,0',
-        include_adjacency: 'true',
-        children_number: '2',
-        page_number: '0',
-        categories_filter_ids: 'class::2,class::4,free_cancellation::1'
-    },
-    headers: {
-        'X-RapidAPI-Key': 'eb62a4e22cmsh6fa4648e0b05b79p1e8afdjsnfdac33378d64',
-        'X-RapidAPI-Host': 'booking-com.p.rapidapi.com'
-    }
-};
-
-
-export const getHotels = async () => {
+export const getHotels = async (cin, cout, adult, child) => {
+    const options = {
+        method: 'GET',
+        url: 'https://booking-com.p.rapidapi.com/v1/hotels/search-by-coordinates',
+        params: {
+            units: 'metric',
+            room_number: '1',
+            longitude: '79.8612',
+            latitude: '6.9271',
+            filter_by_currency: 'AED',
+            order_by: 'class_descending',
+            locale: 'en-gb',
+            checkout_date: cout,
+            adults_number: adult,
+            checkin_date: cin,
+            children_ages: '5,0',
+            include_adjacency: 'true',
+            children_number: child,
+            page_number: '0',
+            categories_filter_ids: 'class::2,class::4,free_cancellation::1'
+        },
+        headers: {
+            'X-RapidAPI-Key': 'eb62a4e22cmsh6fa4648e0b05b79p1e8afdjsnfdac33378d64',
+            'X-RapidAPI-Host': 'booking-com.p.rapidapi.com'
+        }
+    };
     try {
         console.log("working");
         const response = await axios.request(options);
